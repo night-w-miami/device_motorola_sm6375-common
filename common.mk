276409a18b595cd5c96cb002de2816ee86ac1dc8
youngherna <hernan.mauricio16@gmail.com>
2024-10-28 21:33:51 +0000

    Lineageos

--- common.mk
+++ common.mk
@@ -20,7 +20,7 @@ $(call inherit-product-if-exists, vendor/certification/config.mk)
 
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
-    $(LOCAL_PATH)/overlay-aicp
+    $(LOCAL_PATH)/overlay-lineage
 
 PRODUCT_ENFORCE_RRO_TARGETS := *
 
@@ -125,10 +125,6 @@ PRODUCT_PACKAGES += \
     android.hardware.bluetooth@1.0.vendor \
     android.hardware.bluetooth.audio@2.1-impl \
     libbthost_if \
-    libbthost_if.vendor \
-    libldacBT_bco.vendor \
-    com.qualcomm.qti.bluetooth_audio@1.0 \
-    com.qualcomm.qti.bluetooth_audio@1.0.vendor \
     vendor.qti.hardware.bluetooth_audio@2.1.vendor \
     vendor.qti.hardware.btconfigstore@1.0.vendor \
     vendor.qti.hardware.btconfigstore@2.0.vendor
