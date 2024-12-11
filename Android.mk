363db11ecf09b4f6221c13145fcb5921f714fe6f
youngherna <hernan.mauricio16@gmail.com>
2024-12-11 20:19:23 +0000

    sm6375-common: add miami support

--- Android.mk
+++ Android.mk
@@ -6,7 +6,7 @@
 
 LOCAL_PATH := $(call my-dir)
 
-ifneq ($(filter bangkk fogos,$(TARGET_DEVICE)),)
+ifneq ($(filter bangkk fogos miami,$(TARGET_DEVICE)),)
 subdir_makefiles=$(call first-makefiles-under,$(LOCAL_PATH))
 $(foreach mk,$(subdir_makefiles),$(info including $(mk) ...)$(eval include $(mk)))
 
